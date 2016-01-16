48f73f1c9224fb9d894e9fed50a14e936b28eb83
anniebsquared <andrea.bartholomew1@gmail.com>
2016-01-16 03:31:15 +0000

    more inital setup

--- formulamommy.js
+++ formulamommy.js
@@ -1,23 +1,21 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+FlowRouter.route('/',{
+  action: function() {
+    BlazeLayout.render("layout", {content: "home"});
+  }
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Blog.config({
+  title: "Meteor Blog Example",
+  syntaxHighlighting: true,
+  syntaxHighlightingTheme: 'github',
+  blogIndexTemplate: 'blog',
+  blogShowTemplate: 'show',
+  comments: {
+    disqusShortname: 'blogpackage'
+  },
+  rss: {
+    title: 'Example Blog App',
+    description: 'This is an example application for meteor-blog package'
+  }
+});
